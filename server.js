10e427d0088c0cb19ccc3762b9703bf5f879622d
rtake <rtake1110@gmail.com>
2023-05-03 21:36:47 +0900

    feat: update server.js

--- server.js
+++ server.js
@@ -5,8 +5,15 @@ app.get('/', (req, res) => {
     res.status(200).send('hello world\n');
 });
 
+
+/*
 app.get('/user/:id', (req, res) => {
     res.status(200).send(req.params.id);
+});
+*/
+
+app.use((err, req, res, next) => {
+    res.status(500).send('Internal Server Error');
 });
 
 app.listen(3000, () => {
