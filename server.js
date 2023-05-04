f86c66abb18619c0c8956b7ef12f0b3d395173bf
rtake <rtake1110@gmail.com>
2023-05-04 18:00:55 +0900

    feat: implement Create to Redis

--- server.js
+++ server.js
@@ -1,5 +1,22 @@
+const Redis = require('ioredis');
 const express = require('express');
 const app = express();
+
+const redis = new Redis({
+    port: 6379,
+    host: 'localhost',
+    password: process.env.REDIS_PASSWORD,
+    enableOfflineQueue: false
+});
+
+const init = async() => {
+    await Promise.all([
+        redis.set('users:1', JSON.stringify({ id: 1, name: 'alpha'})),
+        redis.set('users:2', JSON.stringify({ id: 2, name: 'bravo'})),
+        redis.set('users:3', JSON.stringify({ id: 3, name: 'charlie'})),
+        redis.set('users:4', JSON.stringify({ id: 4, name: 'delta'}))
+    ]);
+};
 
 app.get('/', (req, res) => {
     res.status(200).send('hello world\n');
@@ -9,11 +26,20 @@ app.get('/user/:id', (req, res) => {
     res.status(200).send(req.params.id);
 });
 
-// Error handling
-app.use((err, req, res, next) => {
-    res.status(500).send('Internal Server Error');
-});
+redis.once('ready', async () => {
+    try {
+        await init();
 
-app.listen(3000, () => {
-    console.log('start listening');
-});
+        app.listen(3000, () => {
+            console.log('start listening');
+        });
+    } catch {
+        console.error(err);
+        process.exit(1);
+    }
+})
+
+redis.on('error', (err) => {
+    console.error(err);
+    process.exit(1);
+})
