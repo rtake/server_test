5f58020fc2f3d24a49725aa5fe92b807656c1b48
rtake <rtake1110@gmail.com>
2023-05-05 03:48:08 +0900

    test: delete unused modules

--- handlers/users.js
+++ handlers/users.js
@@ -1,49 +1,22 @@
-const redis = require('./lib/redis')
-
-redis.once('ready', async () => {
-    try {
-        await redis.init();
-
-        app.get('/', (req, res) => {
-            res.render(path.join(__dirname, 'views', 'index.ejs'));
-        });        
-
-
-        app.listen(3000, () => {
-            console.log('start listening');
-        });
-    } catch {
-        console.error(err);
-        process.exit(1);
-    }
-});
+const redis = require('../lib/redis')
 
 const getUser = async (req) => {
     const key = `users:${req.params.id}`;
-    const val = await redis.get(key);
+    const val = await redis.getClient().get(key);
     const user = JSON.parse(val);
     return user;
 }
 
-app.get('/user/id:', async (req, res) => {
-    try {
-        const user = getUser(req);
-        res.status(200).json(user);
-    } catch {
-        console.error(err);
-        res.status(500).send(('internal error'));
-    }
-});
-
 const getUsers = async () => {
-    const stream = redis.scanStream({
+    const stream = redis.getClient().scanStream({
         match: 'users:*',
         count: 2
     });
 
+    const users = [];
     for await (const resultKeys of stream) {
         for (const key of resultKeys) {
-            const value = await redis.get(key);
+            const value = await redis.getClient().get(key);
             const user = JSON.parse(value);
             users.push(user);
         }
@@ -51,15 +24,6 @@ const getUsers = async () => {
 
     return { users: users };
 }
-
-app.get('/users', async (req, res) => {
-    try {
-        const locals = await getUsers(req);
-        res.render(path.join(__dirname, 'views', 'users.ejs'), locals);
-    } catch {
-        console.error(err);
-    }
-});
 
 exports.getUser = getUser;
 exports.getUsers = getUsers;
